ade401f4d003cd9ce7f76b215bb71b54225f4790
VisalYun <yunvisal18@kit.edu.kh>
2021-11-12 08:50:23 +0700

    done crop detected face and done demo of send to server

---
+++ Assets/Scripts/BaseCamera.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using OpenCvSharp;
+
+/// <summary>
+/// Base WebCamera class that takes care about video capturing.
+/// Is intended to be sub-classed and partially overridden to get
+/// desired behavior in the user Unity script
+/// </summary>
+public abstract class BaseCamera : MonoBehaviour
+{
+    /// <summary>
+    /// Target surface to render WebCam stream
+    /// </summary>
+    public GameObject Surface;
+    public RawImage CaptureImage;
+    public Camera camera;
+    public bool isTaken = false;
+    public bool isDetected = true;
+
+    private Texture2D webCamTexture = null;
+    private RenderTexture rt = null;
+    private Texture2D renderedTexture = null;
+    private UnityEngine.Rect rect;
+    private NRKernal.NRRGBCamTextureYUV YuvCamTexture { get; set; }
+    int resWidth = 1920;
+    int resHeight = 1080;
+
+    protected OpenCvSharp.Unity.TextureConversionParams TextureParameters { get; private set; }
+
+
+    /// <summary>
+    /// Default initializer for MonoBehavior sub-classes
+    /// </summary>
+    protected virtual void Awake()
+    {
+        rt = new RenderTexture(resWidth, resHeight, 24);
+        webCamTexture = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        rect = new UnityEngine.Rect(0, 0, resWidth, resHeight);
+
+        YuvCamTexture = new NRKernal.NRRGBCamTextureYUV();
+        BindYuvTexture(YuvCamTexture.GetTexture());
+        YuvCamTexture.Play();
+    }
+
+
+    /// <summary>
+    /// Updates web camera texture
+    /// </summary>
+    private void Update()
+    {
+        // Debug.Log(isTaken);
+        // Debug.Log(webCamTexture != null);
+        if (webCamTexture != null && !isTaken)
+        {
+            // this must be called continuously
+            isTaken = true;
+            BindYuvTexture(YuvCamTexture.GetTexture());
+            StartCoroutine(takeScreenShot());
+
+            // process texture with whatever method sub-class might have in mind
+            if (ProcessTexture(webCamTexture, ref renderedTexture))
+            {
+                //isDetected = false;
+                RenderFrame();
+                //ProcessImageToServer();
+                if (isDetected)
+                {
+                    isDetected = false;
+                    ProcessImageToServer();
+                }
+            }
+        }
+    }
+
+
+    private void BindYuvTexture(NRKernal.NRRGBCamTextureYUV.YUVTextureFrame frame)
+    {
+        CaptureImage.material.SetTexture("_MainTex", frame.textureY);
+        CaptureImage.material.SetTexture("_UTex", frame.textureU);
+        CaptureImage.material.SetTexture("_VTex", frame.textureV);
+    }
+
+
+    IEnumerator takeScreenShot()
+    {
+        yield return new WaitForEndOfFrame();
+
+        camera.targetTexture = rt;
+        camera.Render();
+        RenderTexture.active = rt;
+        webCamTexture.ReadPixels(rect, 0, 0);
+        //camera.targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        //Destroy(rt);
+    }
+
+
+    /// <summary>
+    /// Processes current texture
+    /// This function is intended to be overridden by sub-classes
+    /// </summary>
+    /// <param name="input">Input WebCamTexture object</param>
+    /// <param name="output">Output Texture2D object</param>
+    /// <returns>True if anything has been processed, false if output didn't change</returns>
+    protected abstract bool ProcessTexture(Texture2D input, ref Texture2D output);
+
+    protected abstract void ProcessImageToServer();
+
+
+    /// <summary>
+    /// Renders frame onto the surface
+    /// </summary>
+    private void RenderFrame()
+    {
+        if (renderedTexture != null)
+        {
+            // apply
+            Surface.GetComponent<RawImage>().texture = renderedTexture;
+            isTaken = false;
+        }
+    }
+}
---
+++ Assets/Scripts/FaceDetector.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using OpenCvSharp;
+
+public class FaceDetector : BaseCamera
+{
+    public TextAsset faces;
+    public TextAsset eyes;
+    public TextAsset shapes;
+    public RawImage FaceInfo;
+    public Button DoneBtn;
+    public Text StatusText;
+
+    private FaceProcessorLive<WebCamTexture> processor;
+
+    private List<UnityEngine.Texture2D> FaceTextures = new List<UnityEngine.Texture2D>();
+
+    /// <summary>
+    /// Default initializer for MonoBehavior sub-classes
+    /// </summary>
+    protected override void Awake()
+    {
+        base.Awake();
+
+        byte[] shapeDat = shapes.bytes;
+        if (shapeDat.Length == 0)
+        {
+            string errorMessage =
+                "In order to have Face Landmarks working you must download special pre-trained shape predictor " +
+                "available for free via DLib library website and replace a placeholder file located at " +
+                "\"OpenCV+Unity/Assets/Resources/shape_predictor_68_face_landmarks.bytes\"\n\n" +
+                "Without shape predictor demo will only detect face rects.";
+
+#if UNITY_EDITOR
+				// query user to download the proper shape predictor
+				if (UnityEditor.EditorUtility.DisplayDialog("Shape predictor data missing", errorMessage, "Download", "OK, process with face rects only"))
+					Application.OpenURL("http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2");
+#else
+            UnityEngine.Debug.Log(errorMessage);
+#endif
+        }
+
+        processor = new FaceProcessorLive<WebCamTexture>();
+        processor.Initialize(faces.text, eyes.text, shapes.bytes);
+
+        // data stabilizer - affects face rects, face landmarks etc.
+        processor.DataStabilizer.Enabled = true;        // enable stabilizer
+        processor.DataStabilizer.Threshold = 2.0;       // threshold value in pixels
+        processor.DataStabilizer.SamplesCount = 2;      // how many samples do we need to compute stable data
+
+        // performance data - some tricks to make it work faster
+        processor.Performance.Downscale = 256;          // processed image is pre-scaled down to N px by long side
+        processor.Performance.SkipRate = 0;             // we actually process only each Nth frame (and every frame for skipRate = 0)
+    }
+
+    /// <summary>
+    /// Per-frame video capture processor
+    /// </summary>
+    protected override bool ProcessTexture(Texture2D input, ref Texture2D output)
+    {
+        if (base.isDetected)
+        {
+            StatusText.text = "Detecting....................";
+        }
+
+        // detect everything we're interested in
+        processor.ProcessTexture(input, TextureParameters);
+
+        // mark detected objects
+        FaceTextures = processor.MarkDetected();
+
+        // processor.Image now holds data we'd like to visualize
+        output = OpenCvSharp.Unity.MatToTexture(processor.Image, output);   // if output is valid texture it's buffer will be re-used, otherwise it will be re-created
+
+        return true;
+    }
+
+    protected override void ProcessImageToServer()
+    {
+        if (FaceTextures.Count == 0)
+        {
+            base.isDetected = true;
+            return;
+        }
+
+        Texture2D texture = FaceTextures[0];
+        byte[] imgBytes = texture.EncodeToPNG();
+        //System.IO.File.WriteAllBytes(Application.dataPath + "/temp.jpg", imgBytes);
+        StartCoroutine(RecognizeFace(texture));
+    }
+
+
+    IEnumerator RecognizeFace(Texture2D texture)
+    {
+        StatusText.text = "Sending to Server.......";
+        Debug.Log("Sending to Server.......");
+
+        yield return new WaitForSeconds(10);
+        StatusText.text = "Done!!!!";
+        Debug.Log("Done!!!!");
+
+        DoneBtn.gameObject.SetActive(true);
+        FaceInfo.gameObject.SetActive(true);
+        FaceInfo.texture = texture;
+    }
+
+    public void BackToDetect()
+    {
+        FaceInfo.gameObject.SetActive(false);
+        DoneBtn.gameObject.SetActive(false);
+        base.isDetected = true;
+    }
+}
